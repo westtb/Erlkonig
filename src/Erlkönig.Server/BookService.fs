f09798324c7b192e84480a49f833fdb088403cef
Uluç Şengil <u.sengil@mail.utoronto.ca>
2020-03-19 20:25:17 -0400

    read from local save file

--- src/Erlkönig.Server/BookService.fs
+++ src/Erlkönig.Server/BookService.fs
@@ -6,44 +6,63 @@ open Microsoft.AspNetCore.Hosting
 open Bolero
 open Bolero.Remoting
 open Bolero.Remoting.Server
+open PKHeX.Core
 open Erlkönig
 
 type BookService(ctx: IRemoteContext, env: IWebHostEnvironment) =
     inherit RemoteHandler<Client.Main.BookService>()
 
-    let books =
-        Path.Combine(env.ContentRootPath, "data/books.json")
-        |> File.ReadAllText
-        |> Json.Deserialize<Client.Main.Book[]>
-        |> ResizeArray
+    let saved = FileUtil.GetSupportedFile(Path.Combine(env.ContentRootPath, "data/hg.sav"))
+
+    let parsePokemon (pkm : PKM) : Client.Main.Pokemon =
+        { species = pkm.Species
+          speciesName = SpeciesName.GetSpeciesName(pkm.Species, int LanguageID.English)
+          altform = 0
+          nickname = if pkm.IsNicknamed then Some pkm.Nickname else None
+          ability = Some pkm.Ability
+          abilityName = Enum.GetName(typeof<Ability>, pkm.Ability)
+          heldItem = Some pkm.HeldItem
+          level = pkm.CurrentLevel
+          data = pkm.Data }
+
+    let pkmExists (pkm : PKM) = pkm.Species <> 0
+    let parseCollect = Seq.filter pkmExists >> List.ofSeq >> List.map parsePokemon
+
+    let readParty (saveFile : SaveFile) = saveFile.PartyData |> parseCollect
+
+    let readBox (saveFile : SaveFile) = saveFile.GetBoxData >> parseCollect
+
+    let readAllPokemon (saveFile : SaveFile) =
+        Map.empty
+        |> Map.add "Party" (readParty saveFile)
+        |> List.fold (fun boxList boxId ->
+            Map.add
+                (saveFile.GetBoxName boxId)
+                (readBox saveFile boxId)
+                boxList
+        )
+        <| [ 0 .. (saveFile.BoxCount - 1)]
 
     override this.Handler =
-        {
-            getBooks = ctx.Authorize <| fun () -> async {
-                return books.ToArray()
-            }
+        { readSave =
+              ctx.Authorize <| fun () ->
+                  async {
+                      return if (saved :? SaveFile) then
+                                 readAllPokemon (saved :?> SaveFile)
+                             else
+                                 Map.empty
+                  }
 
-            addBook = ctx.Authorize <| fun book -> async {
-                books.Add(book)
-            }
+          signIn =
+              fun (username, password) ->
+                  async {
+                      if password = "password" then
+                          do! ctx.HttpContext.AsyncSignIn(username, TimeSpan.FromDays(365.))
+                          return Some username
+                      else
+                          return None
+                  }
 
-            removeBookByIsbn = ctx.Authorize <| fun isbn -> async {
-                books.RemoveAll(fun b -> b.isbn = isbn) |> ignore
-            }
+          signOut = fun () -> async { return! ctx.HttpContext.AsyncSignOut() }
 
-            signIn = fun (username, password) -> async {
-                if password = "password" then
-                    do! ctx.HttpContext.AsyncSignIn(username, TimeSpan.FromDays(365.))
-                    return Some username
-                else
-                    return None
-            }
-
-            signOut = fun () -> async {
-                return! ctx.HttpContext.AsyncSignOut()
-            }
-
-            getUsername = ctx.Authorize <| fun () -> async {
-                return ctx.HttpContext.User.Identity.Name
-            }
-        }
+          getUsername = ctx.Authorize <| fun () -> async { return ctx.HttpContext.User.Identity.Name } }
